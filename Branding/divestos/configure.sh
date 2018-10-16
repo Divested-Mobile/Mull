880f8179030884dc923442ce1415aec9c3fd7a12
Tad <tad@spotco.us>
2018-10-16 01:06:19 -0400

    Rebrand

--- Branding/divestos/configure.sh
+++ Branding/divestos/configure.sh
@@ -3,7 +3,7 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 ANDROID_PACKAGE_NAME=us.spotco.fennec_dos
-MOZ_APP_DISPLAYNAME="Fennec DOS ESR"
+MOZ_APP_DISPLAYNAME="Mull"
 MOZ_APP_ANDROID_VERSION_CODE=1603000
 MOZ_UPDATER=
 MOZ_ANDROID_ANR_REPORTER=
